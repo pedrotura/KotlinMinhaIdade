7c89607da428870e0741447822332362ec66f95c
pedrotura <phgt132@gmail.com>
2024-10-28 22:19:49 -0300

    Finalizando lógica de incremento

--- app/src/main/java/minhaidade/pedrotura/com/github/MainActivity.kt
+++ app/src/main/java/minhaidade/pedrotura/com/github/MainActivity.kt
@@ -6,8 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -15,6 +18,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,18 +39,19 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxSize(),
                         color = MaterialTheme.colorScheme.background
                 ) {
-
-
+                    CounterScreen()
                 }
             }
         }
     }
 }
 
-@Preview()
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun CounterScreen() {
-    var idade = 0
+    var idade = remember{
+        mutableStateOf(0)
+    }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -61,14 +67,16 @@ fun CounterScreen() {
             text = "Aperte os botões para informar a sua idade.",
             fontSize = 12.sp
         )
+        Spacer(modifier = Modifier.height(32.dp))
         Text(
-            text = "21",
+            text = "${idade.value}",
             fontSize = 48.sp,
             fontWeight = FontWeight.Bold
         )
+        Spacer(modifier = Modifier.height(32.dp))
         Row() {
             Button(
-                onClick = {},
+                onClick = {idade.value--},
                 modifier = Modifier.size(84.dp),
                 shape = RoundedCornerShape(8.dp),
                 colors = ButtonDefaults.buttonColors(Color(0xFFAD1F4E))
@@ -78,8 +86,9 @@ fun CounterScreen() {
                     fontSize = 40.sp
                 )
             }
+            Spacer(modifier = Modifier.width(32.dp))
             Button(
-                onClick = {},
+                onClick = {idade.value++},
                 modifier = Modifier.size(84.dp),
                 shape = RoundedCornerShape(8.dp),
                 colors = ButtonDefaults.buttonColors(Color(0xFFAD1F4E))
